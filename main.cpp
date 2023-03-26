3cc854acd8e759702d2995f24a8630d31de1ad7e
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-03-26 13:00:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main(){
     float a, b, c;
     cout << "inserisci tre valori reali:\n";
     cin >> a >> b >> c;
-    cout <<" Area triangolo=" << (a*b)/2 << "\n";
-    cout <<" Area quadrato=" << a * a<< "\n";
-    cout <<" Area rettangolo=" << a * b <<"\n";
-    cout <<" Area trapezio=" << ((a+b)*c)/2;
+    cout << (a*b)/2 << "\n";
+    cout << a * a<< "\n";
+    cout << a * b <<"\n";
+    cout << ((a+b)*c)/2;
     return 0;
 }
